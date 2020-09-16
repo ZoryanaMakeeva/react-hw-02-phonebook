563e1f8c24a60fb580950eae069948dbae739988
Zoriana <makieieva.dev@gmail.com>
2020-09-16 12:45:17 +0300

    Updates

---
+++ precache-manifest.1fbacba4e205f92a4cc6d5c7daa08850.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e1b934e824186dfa87a615c86fdbd2",
+    "url": "/react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "ac05b304632c74f71e37",
+    "url": "/react-hw-02-phonebook/static/css/main.b989cb4f.chunk.css"
+  },
+  {
+    "revision": "2194b4e2a4a2472b3f4a",
+    "url": "/react-hw-02-phonebook/static/js/2.110f48f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-02-phonebook/static/js/2.110f48f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac05b304632c74f71e37",
+    "url": "/react-hw-02-phonebook/static/js/main.0cd89352.chunk.js"
+  },
+  {
+    "revision": "6f482f3e5b3d66793c08",
+    "url": "/react-hw-02-phonebook/static/js/runtime-main.2c5ed82f.js"
+  }
+]);
